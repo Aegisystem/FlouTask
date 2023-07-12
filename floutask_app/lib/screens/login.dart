a9f4c637c29b5451a3d89d136b6bf127b2c2d8da
Steven Correa <97488936+Aegisystem@users.noreply.github.com>
2023-07-12 18:38:43 -0500

    Google Sign In Button, complete login screen is left

--- floutask_app/lib/screens/login.dart
+++ floutask_app/lib/screens/login.dart
@@ -5,21 +5,38 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Login extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     Firebase.initializeApp();
     return Scaffold(
+      backgroundColor: Color(0xFFE2C6A9), // Establece el fondo de la pantalla
       body: Center(
-        child: MaterialButton(
-          onPressed: () async {
-            User? user = await Authenticator.iniciarSesion(context: context);
-            print(user?.displayName);
-          },
-          color: Colors.blue,
-          child: Icon(FontAwesomeIcons.google),
-          textColor: Colors.white,
-        )
+        child: FractionallySizedBox(
+          widthFactor: 0.8, // Limita el ancho al 80% de la pantalla
+          child: MaterialButton(
+            onPressed: () async {
+              User? user = await Authenticator.iniciarSesion(context: context);
+              print(user?.displayName);
+            },
+            color: Colors.white, // Establece el color de fondo del botón
+            height: 50, // Ajusta la altura del botón
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(FontAwesomeIcons.google),
+                SizedBox(width: 10),
+                Text(
+                  'Iniciar Sesión con Google',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.brown, // Establece el color del texto
+                  ),
+                ),
+              ],
+            ),
+            textColor: Colors.brown,
+          ),
+        ),
       ),
     );
   }

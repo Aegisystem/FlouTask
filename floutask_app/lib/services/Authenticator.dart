78e1add3bf436e87a9e3178ddc42fa1ea721436c
sergito1x <2220201015@estudiantesunibague.edu.co>
2023-07-14 18:56:35 -0500

    "FIREBASE DATABASE ADDED"

--- floutask_app/lib/services/Authenticator.dart
+++ floutask_app/lib/services/Authenticator.dart
@@ -1,8 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-class Authenticator{
+class Authenticator {
   static Future<User?> iniciarSesion({required BuildContext context}) async {
     FirebaseAuth authenticator = FirebaseAuth.instance;
     User? user;
@@ -10,21 +11,39 @@ class Authenticator{
     GoogleSignIn googleSignIn = GoogleSignIn();
     GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
 
-    if(googleSignInAccount != null) {
+    if (googleSignInAccount != null) {
       GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
       AuthCredential credential = GoogleAuthProvider.credential(
-        accessToken: googleSignInAuthentication.accessToken,
-        idToken: googleSignInAuthentication.idToken
+          accessToken: googleSignInAuthentication.accessToken,
+          idToken: googleSignInAuthentication.idToken
       );
-      try{
+      try {
         UserCredential userCredential = await authenticator.signInWithCredential(credential);
 
         user = userCredential.user;
+
+        // Crear el documento del usuario en Firestore
+        await createUserDocument(user!);
 
         return user;
       } on FirebaseAuthException catch (e) {
         print("Error en la autenticación");
       }
+    }
+  }
+
+  static Future<void> createUserDocument(User user) async {
+    DocumentReference userRef = FirebaseFirestore.instance.collection('users').doc(user.uid);
+
+    DocumentSnapshot userSnapshot = await userRef.get();
+    if (!userSnapshot.exists) {
+      await userRef.set({
+        'name': user.displayName,
+        'email': user.email,
+        'photoURL': user.photoURL,
+        'uid': user.uid,
+        // Otras propiedades del perfil del usuario
+      });
     }
   }
 }
